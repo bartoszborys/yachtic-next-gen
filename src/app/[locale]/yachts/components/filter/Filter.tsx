0754e629e8740f49839c4eef2997e3f5a970de77
Bartosz Borys <borys@cyberstudio.pl>
2024-03-31 09:28:02 +0200

    Rename components

--- src/app/[locale]/yachts/components/filter/Filter.tsx
+++ src/app/[locale]/yachts/components/filter/Filter.tsx
@@ -1,10 +1,10 @@
-import Checkbox from "./client/FilterCheckbox";
-import DateInput from "./client/FilterDate";
-import Searches from "./client/FilterSearches";
+import FilterCheckbox from "./client/FilterCheckbox";
+import FilterDate from "./client/FilterDate";
+import FilterSearches from "./client/FilterSearches";
 import FilterSelect from "./client/FilterSelect";
 import FilterText from "./client/FilterText";
-import RangeSlider from "./client/FilterRangeSlider";
-import MinMaxRangeNumber from "./client/FilterMinMaxRange";
+import FilterRangeSlider from "./client/FilterRangeSlider";
+import FilterMinMaxRange from "./client/FilterMinMaxRange";
 import FilterAutocomplete from "./client/FilterAutocomplete";
 import {
     getSearches,
@@ -27,7 +27,7 @@ export default async function Filter() {
         <>
             <div className="bg-[#00a0e3] flex flex-col p-5 text-xs">
                 <FilterSelect filterName="countries[0][id]" options={countries} description="COUNTRIES" />
-                <DateInput filterName="date" text="Date" />
+                <FilterDate filterName="date" text="Date" />
                 <div className="flex flex-col">
                     <span>Days</span>
                     <FilterText filterName="days" />
@@ -36,30 +36,30 @@ export default async function Filter() {
             <div className="bg-white flex flex-col p-5 text-xs">
                 <FilterSelect filterName="kindId" options={kinds} description="BOAT TYPE" />
 
-                <RangeSlider min={1} max={12} filterName={['personsMin', 'personsMax']} text="Persons"/>
-                <RangeSlider min={1} max={12} filterName={['berthsMin', 'berthsMax']} text="Berths"/>
-                <RangeSlider min={1} max={6} filterName={['cabinsMin', 'cabinsMax']} text="Cabins"/>
-                <RangeSlider min={1} max={6} filterName={['toiletsMin', 'toiletsMax']} text="Toilets"/>
+                <FilterRangeSlider min={1} max={12} filterName={['personsMin', 'personsMax']} text="Persons"/>
+                <FilterRangeSlider min={1} max={12} filterName={['berthsMin', 'berthsMax']} text="Berths"/>
+                <FilterRangeSlider min={1} max={6} filterName={['cabinsMin', 'cabinsMax']} text="Cabins"/>
+                <FilterRangeSlider min={1} max={6} filterName={['toiletsMin', 'toiletsMax']} text="Toilets"/>
 
-                <Checkbox filterName="recommendedFirst" text="Recommended first" />
-                <Checkbox filterName="lowFirstInstallment" text="Low first rate" />
+                <FilterCheckbox filterName="recommendedFirst" text="Recommended first" />
+                <FilterCheckbox filterName="lowFirstInstallment" text="Low first rate" />
 
                 <div className="my-2">
                     <span>WYPOSAŻENIE I DODATKI</span>
-                    <Searches data={searchesData} />
+                    <FilterSearches data={searchesData} />
                 </div>
 
-                <Checkbox filterName="haveLicense" text="Bareboat" />
+                <FilterCheckbox filterName="haveLicense" text="Bareboat" />
 
-                <Checkbox filterName="needSkipper" text="I need a skipper" />
+                <FilterCheckbox filterName="needSkipper" text="I need a skipper" />
 
                 <FilterSelect filterName="ratingMin" options={rates.map((rating) => ({ name: rating.name, value: rating.id }))} description="RATE NOT LESS THAN" />
 
                 <FilterSelect filterName="yearMin" options={years} description="NOT OLDER THAN" />
 
-                <MinMaxRangeNumber label="PRICE (€)" filterName={['personsMin', 'personsMax']} placeholder={['From...', 'To...']}/>
+                <FilterMinMaxRange label="PRICE (€)" filterName={['personsMin', 'personsMax']} placeholder={['From...', 'To...']}/>
                 
-                <MinMaxRangeNumber label="LENGTH (M)" filterName={['lengthMin', 'lengthMax']} placeholder={['From...', 'To...']}/>
+                <FilterMinMaxRange label="LENGTH (M)" filterName={['lengthMin', 'lengthMax']} placeholder={['From...', 'To...']}/>
                 <div>
                     <span>NAME OR MODEL</span>
                     <FilterText filterName="name" />
