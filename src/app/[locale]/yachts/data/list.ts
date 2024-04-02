4087d6594bb59a4c11144e17bc6495c98424ce13
Bartosz Borys <borys@cyberstudio.pl>
2024-04-02 15:12:34 +0200

    set redirect to production

--- src/app/[locale]/yachts/data/list.ts
+++ src/app/[locale]/yachts/data/list.ts
@@ -5,7 +5,5 @@ export async function getYachts(params: URLSearchParams): Promise<YachtsData[]> 
     const url = `${API_URL}/yachts?currencyId=2&languageName=yachts&languageId=2&currencyId=2&page=2&sort=priceAsc&${params.toString()}`
     //const url = "http://localhost:8080/yachts?currencyId=2&languageName=yachts&languageId=2&currencyId=2&date=2024-04-06&days=7&page=2&sort=priceAsc&kindId=&companyId=&recommendedFirst=false&bathroomsMin=&bathroomsMax=&cabinsMin=&cabinsMax=&headsMin=&headsMax=&berthsMin=&berthsMax=&personsMin=&personsMax=&ratingMin=&yearMin=&priceMin=&priceMax=&lengthMin=&lengthMax=&name=&lowFirstInstallment=false&haveLicense=false&needSkipper=false&hidePinned=false&utm_campaign=&utm_term=&utm_id=&utm_source=&utm_medium=&utm_content=";
     
-    console.log(noCorsFetch, url);
-    
-    return await (await fetch(url)).json();
+    return await (await noCorsFetch(url)).json();
 }
--- src/navigation.ts
+++ src/navigation.ts
@@ -3,4 +3,4 @@ import getPathnames from './getPathnames';
 export const locales = ['en', 'pl', 'de'];
 export const localePrefix = "as-needed";
 export const pathnames = getPathnames();
-export const toExternalHref = (url: string) => `http://localhost:8083/${url}`
+export const toExternalHref = (url: string) => `https://yachtic.com/${url}`
