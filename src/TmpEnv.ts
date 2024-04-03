c5f843961a65dd748bc785a9b603c95e3eee1253
Bartosz Borys <borys@cyberstudio.pl>
2024-04-03 09:53:20 +0200

    use production api

--- src/TmpEnv.ts
+++ src/TmpEnv.ts
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://localhost:8080";
+export const API_URL = "https://api.yachtic.com";
 
 export async function noCorsFetch(input: URL | RequestInfo, init?: RequestInit | undefined): Promise<Response> {
     return await fetch(input, {...init, next: {revalidate: 3600}});
